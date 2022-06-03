9d6a02566cbbba5ba3f0aa3f3cb60bd34162069a
Aydeke <aydeke2@gmail.com>
2022-06-03 21:23:43 +0600

    Add server.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require("./1-simple-event");
-require("./2-multiple");
-require("./2.5-cyrillic");
-require("./3-callback");
-require("./4-args");
+// require("./1-simple-event");
+// require("./2-multiple");
+// require("./2.5-cyrillic");
+// require("./3-callback");
+// require("./4-args");
+require("./server");
---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  // const { method, url, headers } = req;
+  // console.log("Метод:", method); // GET, POST, PUT, DELETE
+  // console.log("URL:", url);
+  // console.log(headers["user-agent"]);
+
+  const data = url.parse(req.url, true);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html; charset=utf8");
+
+  if (data.query.name) {
+    res.end(`<h1>Привет, ${data.query.name}</h1>`);
+  } else {
+    res.end("<h1 style='color: red'>Привет от сервера!</h1>");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Сервер слушает порт ${PORT}`);
+});
